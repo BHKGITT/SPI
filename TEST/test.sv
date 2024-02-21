071db1788e4b97232f781fbd7b07ad5dafa7cbb7
Hari Krishna <158833808+BHKGITT@users.noreply.github.com>
2024-02-21 12:55:34 +0530

    SPI

--- TEST/test.sv
+++ TEST/test.sv
@@ -28,6 +28,18 @@ function test::new(string name="test",uvm_component parent);
 	super.new(name,parent);
 endfunction
 
+function void test::build_phase(uvm_phase phase);
+	super.build_phase(phase);
+//Create instance for env_config 
+	env_cfg=env_config::type_id::create("env_cfg");
+
+	v_seqh=virtual_sequence::type_id::create("v_seqh");
+	config_spi(); //calling function
+//Create instance for spi_env class
+	envh=spi_env::type_id::create("envh",this);
+
+endfunction
+
 function void test::config_spi();
 	if(has_magent)
 		begin
@@ -58,7 +70,6 @@ function void test::config_spi();
 					env_cfg.s_cfg[i]=s_cfg[i];//pointing env_cfg.s_cfg[] with local s_cfg[] 
 				end
 		end
-
 //Set env_config	
 	uvm_config_db#(env_config)::set(this,"*","ENV_CFG",env_cfg);
 	
@@ -68,29 +79,7 @@ function void test::config_spi();
 	env_cfg.no_of_sagent=no_of_sagent;
 	env_cfg.has_scoreboard=has_scoreboard;
 endfunction
-function void test::build_phase(uvm_phase phase);
-	super.build_phase(phase);
-//Configuring CTRL register
-	ctrl[6:0] = 7'd10;  //Character_length
-	ctrl[7]   =  0;  //Reseved
-	ctrl[8]   =  1;  //Go busy
-	ctrl[9]	  =  1;  //Rx_neg
-	ctrl[10]  =  0;  //Tx_neg
-	ctrl[11]  =  1;  //LSB
-	ctrl[12]  =  1;  //Interrupt Enable
-	ctrl[13]  =  1;  //Auto slave select
-	ctrl[31:14]= 0;  //Reserved
 
-//Setting ctrl register
-	uvm_config_db#(int)::set(this,"*","CTRL",ctrl);	
-
-	v_seqh=virtual_sequence::type_id::create("v_seqh");
-//Create instance for spi_env class
-	envh=spi_env::type_id::create("envh",this);
-//Create instance for env_config 
-	env_cfg=env_config::type_id::create("env_cfg");
-	config_spi(); //calling function
-endfunction
 
 function void test::end_of_elaboration_phase(uvm_phase phase);
 	super.end_of_elaboration_phase(phase);
@@ -101,7 +90,7 @@ endfunction
 class test_1 extends test;
 	`uvm_component_utils(test_1)
 //Declare virtual_seq1 handle
-	virtual_seq1	v_seqh1;		
+	virtual_seq1	v_seqh1;
 
 	extern function new(string name="test_1",uvm_component parent);
 	extern function void build_phase(uvm_phase phase);
@@ -114,6 +103,20 @@ endfunction
 
 function void test_1::build_phase(uvm_phase phase);
 	super.build_phase(phase);
+//Configuring CTRL register
+	ctrl[6:0] = 7'd32;  //Character_length
+	ctrl[7]   =  0;  //Reseved
+	ctrl[8]   =  1;  //Go busy
+	ctrl[9]	  =  1;  //Rx_neg
+	ctrl[10]  =  0;  //Tx_neg
+	ctrl[11]  =  1;  //LSB
+	ctrl[12]  =  1;  //Interrupt Enable
+	ctrl[13]  =  1;  //Auto slave select
+	ctrl[31:14]= 0;  //Reserved
+
+//Setting ctrl register
+	uvm_config_db#(int)::set(this,"*","CTRL",ctrl);	
+
 endfunction
 
 task test_1::run_phase(uvm_phase phase);
@@ -122,6 +125,92 @@ task test_1::run_phase(uvm_phase phase);
 		v_seqh1=virtual_seq1::type_id::create("v_seqh1"); //Creating instance for v_seqh1
 		phase.raise_objection(this);
 		v_seqh1.start(envh.env_v_seqr); // Starting v_seqh1 on env_v_seqr
+		#100;
 		phase.drop_objection(this);
 	end
 endtask
+//////////////////////////////////// TEST-2 //////////////////////////////////////
+class test_2 extends test;
+	`uvm_component_utils(test_2)
+//Declare virtual_seq1 handle
+	virtual_seq1	v_seqh1;		
+	extern function new(string name="test_2",uvm_component parent);
+	extern function void build_phase(uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+endclass
+
+function test_2::new(string name="test_2",uvm_component parent);
+	super.new(name,parent);
+endfunction
+
+function void test_2::build_phase(uvm_phase phase);
+	super.build_phase(phase);
+//Configuring CTRL register
+	ctrl[6:0] = 7'd64;  //Character_length
+	ctrl[7]   =  0;  //Reseved
+	ctrl[8]   =  1;  //Go busy
+	ctrl[9]	  =  1;  //Rx_neg
+	ctrl[10]  =  0;  //Tx_neg
+	ctrl[11]  =  1;  //LSB
+	ctrl[12]  =  1;  //Interrupt Enable
+	ctrl[13]  =  1;  //Auto slave select
+	ctrl[31:14]= 0;  //Reserved
+
+//Setting ctrl register
+	uvm_config_db#(int)::set(this,"*","CTRL",ctrl);	
+endfunction
+
+task test_2::run_phase(uvm_phase phase);
+	super.run_phase(phase);
+	begin
+		v_seqh1=virtual_seq1::type_id::create("v_seqh1"); //Creating instance for v_seqh1
+		phase.raise_objection(this);
+		v_seqh1.start(envh.env_v_seqr); // Starting v_seqh1 on env_v_seqr
+		#100;
+		phase.drop_objection(this);
+	end
+endtask
+
+//////////////////////////////////// TEST-3 //////////////////////////////////////
+class test_3 extends test;
+	`uvm_component_utils(test_3)
+//Declare virtual_seq1 handle
+	virtual_seq1	v_seqh1;		
+//	int ctrl;
+	extern function new(string name="test_3",uvm_component parent);
+	extern function void build_phase(uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+endclass
+
+function test_3::new(string name="test_3",uvm_component parent);
+	super.new(name,parent);
+endfunction
+
+function void test_3::build_phase(uvm_phase phase);
+	super.build_phase(phase);
+//Configuring CTRL register
+	ctrl[6:0] = 7'd127;  //Character_length
+	ctrl[7]   =  0;  //Reseved
+	ctrl[8]   =  1;  //Go busy
+	ctrl[9]	  =  1;  //Rx_neg
+	ctrl[10]  =  0;  //Tx_neg
+	ctrl[11]  =  1;  //LSB
+	ctrl[12]  =  1;  //Interrupt Enable
+	ctrl[13]  =  1;  //Auto slave select
+	ctrl[31:14]= 0;  //Reserved
+
+//Setting ctrl register
+	uvm_config_db#(int)::set(this,"*","CTRL",ctrl);	
+endfunction
+
+task test_3::run_phase(uvm_phase phase);
+	super.run_phase(phase);
+	begin
+		v_seqh1=virtual_seq1::type_id::create("v_seqh1"); //Creating instance for v_seqh1
+		phase.raise_objection(this);
+		v_seqh1.start(envh.env_v_seqr); // Starting v_seqh1 on env_v_seqr
+		#100;
+		phase.drop_objection(this);
+	end
+endtask
+
