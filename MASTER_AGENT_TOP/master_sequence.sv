5395f99542acab523fb0e4c7df4ffcf7d7b84db8
Hari Krishna <158833808+BHKGITT@users.noreply.github.com>
2024-02-21 12:52:21 +0530

    SPI

--- MASTER_AGENT_TOP/master_sequence.sv
+++ MASTER_AGENT_TOP/master_sequence.sv
@@ -30,20 +30,36 @@ task master_seq1::body();
 	req=master_txn::type_id::create("req");
 //For TX0 register
 	start_item(req);
-	assert(req.randomize() with {wb_adr_i==5'h00 ; wb_we_i==1'b1 ; wb_dat_i==1000;});
+	assert(req.randomize() with {wb_adr_i==5'h00 ; wb_we_i==1'b1 ; wb_dat_i==32'd1000;});
 	finish_item(req);
 //For TX1 register
 	start_item(req);
-	assert(req.randomize() with {wb_adr_i==5'h04 ; wb_we_i==1'b1 ; wb_dat_i==1000;});
+	assert(req.randomize() with {wb_adr_i==5'h04 ; wb_we_i==1'b1 ; wb_dat_i==32'd2000;});
 	finish_item(req);
 //For TX2 register
 	start_item(req);
-	assert(req.randomize() with {wb_adr_i==5'h08 ; wb_we_i==1'b1 ; wb_dat_i==1000;});
+	assert(req.randomize() with {wb_adr_i==5'h08 ; wb_we_i==1'b1 ; wb_dat_i==32'd3000;});
 	finish_item(req);
 //For TX3 register
 	start_item(req);
-	assert(req.randomize() with {wb_adr_i==5'h0C ; wb_we_i==1'b1 ; wb_dat_i==1000;});
+	assert(req.randomize() with {wb_adr_i==5'h0C ; wb_we_i==1'b1 ; wb_dat_i==32'd4000;});
 	finish_item(req);
+//For CTRL register
+//	int ctrl_h=ctrl[6:0];
+
+	start_item(req);
+	assert(req.randomize() with{wb_adr_i==5'h10 ; wb_we_i==1'b1 ; wb_dat_i==ctrl;});
+	finish_item(req);
+//For Divider register
+	start_item(req);
+	assert(req.randomize() with{wb_adr_i==5'h14 ; wb_we_i==1'b1 ; wb_dat_i[15:0]==16'b0000_0010; wb_dat_i[31:16]==16'b0000_0000;});
+	finish_item(req);
+//For SS register
+	start_item(req);
+	assert(req.randomize() with{wb_adr_i==5'h18 ; wb_we_i==1'b1 ; wb_dat_i[7:0]==8'b0000_0001; wb_dat_i[31:8]=='b0;});
+	finish_item(req);
+
+#2000;
 //For RX0 register
 	start_item(req);
 	assert(req.randomize() with {wb_adr_i==5'h00 ; wb_we_i==1'b0 ;});
@@ -59,20 +75,6 @@ task master_seq1::body();
 //For RX3 register
 	start_item(req);
 	assert(req.randomize() with{wb_adr_i==5'h0C ; wb_we_i==1'b0 ;});
-	finish_item(req);
-//For CTRL register
-//	int ctrl_h=ctrl[6:0];
-
-	start_item(req);
-	assert(req.randomize() with{wb_adr_i==5'h10 ; wb_we_i==1'b1 ; wb_dat_i==ctrl[6:0];});
-	finish_item(req);
-//For Divider register
-	start_item(req);
-	assert(req.randomize() with{wb_adr_i==5'h14 ; wb_we_i==1'b1 ; wb_dat_i[15:0]==16'b0000_0010; wb_dat_i[31:16]==16'b0000_0000;});
-	finish_item(req);
-//For SS register
-	start_item(req);
-	assert(req.randomize() with{wb_adr_i==5'h18 ; wb_we_i==1'b1 ; wb_dat_i[7:0]==8'b0000_0001; wb_dat_i[31:8]=='b0;});
 	finish_item(req);
 
 //	`uvm_info(get_type_name(),$sformatf("Generated  seqs at MASTER \n %s",req.sprint()),UVM_LOW)
